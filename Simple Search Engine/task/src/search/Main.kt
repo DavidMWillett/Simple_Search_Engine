741f47eea095c68346013918cf770e700ce63ca3
david <david.m.willett@gmail.com>
2020-06-09 17:21:30 +0100

    Stage 3 completed.

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -1,37 +1,51 @@
 package search
 
 fun main() {
-    val people = enterPeople()
-    searchPeople(people)
+    val people = People()
+    loop@ do {
+        println("=== Menu ===")
+        println("1. Find a person")
+        println("2. Print all people")
+        println("0. Exit")
+        val choice = readLine()!!.toInt()
+        println()
+        when (choice) {
+            1 -> people.search()
+            2 -> people.list()
+            0 -> break@loop
+            else -> println("Incorrect option! Try again.")
+        }
+        println()
+    } while (true)
+    println("Bye!")
 }
 
-fun enterPeople(): List<String> {
-    println("Enter the number of people:")
-    val numPeople = readLine()!!.toInt()
-    val people = mutableListOf<String>()
-    println("Enter all people:")
-    repeat(numPeople) {
-        people.add(readLine()!!)
-    }
-    println()
-    return people
-}
+class People {
+    private val people: List<String> = {
+        println("Enter the number of people:")
+        val numPeople = readLine()!!.toInt()
+        val people = mutableListOf<String>()
+        println("Enter all people:")
+        repeat(numPeople) {
+            people.add(readLine()!!)
+        }
+        println()
+        people
+    }()
 
-fun searchPeople(people: List<String>) {
-    println("Enter the number of search queries:")
-    val numQueries = readLine()!!.toInt()
-    println()
-    repeat(numQueries) {
-        println("Enter data to search people:")
+    fun search() {
+        println("Enter a name or email to search all suitable people.")
         val search = readLine()!!
         val found = people.filter { it.contains(search, ignoreCase = true) }
         if (found.isNotEmpty()) {
-            println()
-            println("Found people:")
             found.forEach { println(it) }
         } else {
             println("No matching people found.")
         }
-        println()
+    }
+
+    fun list() {
+        println("=== List of people ===")
+        people.forEach { println(it) }
     }
 }
