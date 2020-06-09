0c3555c7aed07a1d996d27d7bd24849688f18872
david <david.m.willett@gmail.com>
2020-06-09 15:17:42 +0100

    Stage 2 completed.

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -1,19 +1,37 @@
 package search
 
-import java.util.Scanner
-
 fun main() {
-    val database = readLine()!!
-    val keyword = readLine()!!
+    val people = enterPeople()
+    searchPeople(people)
+}
 
-    val scanner = Scanner(database)
-    var index = 0
-    while (scanner.hasNext()) {
-        index++
-        if (scanner.next() == keyword) {
-            println(index)
-            return
-        }
+fun enterPeople(): List<String> {
+    println("Enter the number of people:")
+    val numPeople = readLine()!!.toInt()
+    val people = mutableListOf<String>()
+    println("Enter all people:")
+    repeat(numPeople) {
+        people.add(readLine()!!)
     }
-    println("Not found")
+    println()
+    return people
+}
+
+fun searchPeople(people: List<String>) {
+    println("Enter the number of search queries:")
+    val numQueries = readLine()!!.toInt()
+    println()
+    repeat(numQueries) {
+        println("Enter data to search people:")
+        val search = readLine()!!
+        val found = people.filter { it.contains(search, ignoreCase = true) }
+        if (found.isNotEmpty()) {
+            println()
+            println("Found people:")
+            found.forEach { println(it) }
+        } else {
+            println("No matching people found.")
+        }
+        println()
+    }
 }
